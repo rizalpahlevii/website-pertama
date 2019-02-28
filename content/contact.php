9602234a5d2a1eb58fe3e61e86e67a3917dca68f
Muhammad Rizal Pahlevi <mrizalpahlevi372@gmail.com>
2019-02-28 11:48:18 +0700

    conten commit

---
+++ content/contact.php
@@ -0,0 +1,21 @@
+<div class="contact">
+	
+	<h2>Contact</h2>
+	<table>
+		<tr>
+			<td>Nama</td>
+			<td>:</td>
+			<td>Muhammad Rizal Pahlevi</td>
+		</tr>
+		<tr>
+			<td>No.Hp</td>
+			<td>:</td>
+			<td>081327551680</td>
+		</tr>
+		<tr>
+			<td>E-Mail</td>
+			<td>:</td>
+			<td>Mrizalpahlevi372@gmail.com</td>
+		</tr>
+	</table>
+</div>
---
+++ content/home.php
@@ -0,0 +1,14 @@
+		<h1>Judul Artikel</h1>
+			<p class="tgl">23 Agustus 2018</p>
+			<p class="art">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<p class="art">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
